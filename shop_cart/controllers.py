1336e3052329e365c8d19119c500e3ac69b66c22
David Johnson <szuroveczdavid@gmail.com>
2019-07-20 21:08:29 +0200

    url mapping

--- shop_cart/controllers.py
+++ shop_cart/controllers.py
@@ -17,6 +17,7 @@ class WebService:
         self._environment = Environment(loader=FileSystemLoader(WebService.TEMPLATE_FOLDER))
         self._html_file = self._environment.get_template(html_template_file_name)
 
+
 class HomePage(WebService):
     @cherrypy.expose
     def index(self):
@@ -36,11 +37,12 @@ class ComPairPage:
     def index(self):
         return open(r'views\compair.html')
 
-class ContactPage:
 
+class ContactPage(WebService):
+    #todo layout of the google map should be optimized
     @cherrypy.expose
     def index(self):
-        return open(r'views\contact.html')
+        return self._html_file.render()
 
 
 class ForgetPasswordPage:
@@ -49,8 +51,21 @@ class ForgetPasswordPage:
     def index(self):
         return open(r'views\forget_password.html')
 
+class GridViewPage(WebService):
+
+    @cherrypy.expose
+    def index(self):
+        return self._html_file.render()
+
 
 class GeneralPage(WebService):
+
+    @cherrypy.expose
+    def index(self):
+        return self._html_file.render()
+
+
+class ListViewPage(WebService):
 
     @cherrypy.expose
     def index(self):
@@ -77,6 +92,12 @@ class ProductsPage:
     def index(self):
         return open(r'views\products.html')
 
+class ThreeColPage(WebService):
+
+    @cherrypy.expose
+    def index(self):
+        return self._html_file.render()
+
 
 @cherrypy.popargs('product')
 class ProductDetailsPage:
@@ -85,27 +106,31 @@ class ProductDetailsPage:
         return open(r'views\product-details.html')
 
 
-class RegisterPage:
+class RegisterPage(WebService):
 
     @cherrypy.expose
     def index(self):
-        return open(r'views\register.html')
+        return self._html_file.render()
 
 
 if __name__ == '__main__':
     cherrypy.config.update({'log.screen': True,
                             'log.access_file': '',
                             'log.error_file': 'error.txt'})
+    product_details = "product-details"
+    list_view = "list-view"
+    grid_view = "grid-view"
     WEBAPP = HomePage('index.html')
     WEBAPP.cart = CartPage()
     WEBAPP.compair = ComPairPage()
-    WEBAPP.contact = ContactPage()
+    WEBAPP.contact = ContactPage('contact.html')
+    WEBAPP.list_view = ListViewPage('list-view.html')
+    WEBAPP.grid_view = GridViewPage('grid-view.html')
     WEBAPP.forget_password = ForgetPasswordPage()
     WEBAPP.general = GeneralPage('general.html')
     WEBAPP.header = HeaderPage()
     WEBAPP.login = LoginPage()
-    product_details = "product-details"
     WEBAPP.product_details = ProductDetailsPage()
     WEBAPP.products = ProductsPage()
-    WEBAPP.register = RegisterPage()
+    WEBAPP.register = RegisterPage('register.html')
     cherrypy.quickstart(WEBAPP, '/', config.cherrpy_run_conf)
